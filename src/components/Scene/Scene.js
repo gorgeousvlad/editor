71c13be297cfb1f355b8cf2a6106a54ed3858963
Влад <gorgeousvlad@gmail.com>
2018-03-23 13:27:20 +0300

    drag added

--- src/components/Scene/Scene.js
+++ src/components/Scene/Scene.js
@@ -1,24 +1,40 @@
 import React, { Component } from 'react';
 import './Scene.scss';
-//import Button from'../../elements/Button/Button';
 import ElementFabric from'../../fabric/ElementFabric';
+import Draggable from 'react-draggable';
 
-let fabric = new ElementFabric();
 const elements = [
   {
     element:"button",
     type:"basic",
     text:"Готово"
   }
-]
+];
+let fabric = new ElementFabric(),
+eventLogger = (e: MouseEvent, data: Object) => {
+    console.log('Event: ', e);
+    console.log('Data: ', data);
+  };
 
 export default class Scene extends Component {
   render() {
     return (
       <div className = "scene">
-      {elements.map( (el,index) => {
-        return fabric.create(Object.assign({}, el, {key:`${el.element}-${index}`}))
-      })}
+        {elements.map( (el,index) => {
+          return <Draggable
+            key = {`scene-draggable-${index}`}
+            handle=".handle"
+            defaultPosition={{x: 0, y: 0}}
+            position={null}
+            //grid={[25, 25]}
+            onStart={this.handleStart}
+            onDrag={this.handleDrag}
+            onStop={this.handleStop}>
+            <div className = "handle">
+              {fabric.create(Object.assign({}, el, {key:`${el.element}-${index}`}))}
+            </div>
+          </Draggable>
+        })}
       </div>
     );
   }
